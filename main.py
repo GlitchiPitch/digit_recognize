ab0946bd4e8c986ea56d630e1952744d18235a2d
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-16 09:02:33 +0500

    create a train dataset

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tensorflow import keras
 from keras.layers import Dense, Flatten, Input, Dropout, BatchNormalization
+from keras.datasets import mnist
+from sklearn.model_selection import train_test_split
 
 model = keras.Sequential([
     Input(shape=(28,28,1)),
@@ -14,3 +16,14 @@ model.compile(
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train / 255
+x_test = x_test / 255
+
+y_train_cat = keras.utils.to_categorical(y_train, 10)
+y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(x_train, y_train_cat, test_size=.2)
